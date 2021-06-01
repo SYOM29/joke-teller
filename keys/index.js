b8977f1592985667d4aebc7b92e1a81e967c65bd
Siyovush Kadyrov <siyovush.kadyrov@gmail.com>
2021-06-01 22:49:08 +0300

    Added voicerss

---
+++ keys/index.js
@@ -0,0 +1,9 @@
+import devKeys from './dev.js';
+
+export default devKeys; 
+
+// if(process.env.NODE_ENV === 'production'){
+//     module.exports = require('./prod');
+//   } else {
+//     module.exports = require('./dev');
+//   }
